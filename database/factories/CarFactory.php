bc645b8f21ea4516be1ecf088404928132282f50
Arkadiusz Rodak <arkadiusz.rodak.89@gmail.com>
2022-07-12 22:35:47 +0200

    feat: migrations and models

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Car>
+ */
+class CarFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'make' => $this->faker->lastName(),
+            'model' => $this->faker->colorName(),
+            'year' => $this->faker->numberBetween(1990, 2022),
+        ];
+    }
+}
